d08d039cd5945fda0da75e03d1ab1c4f2e3e0171
mirror <mirror12k@gmail.com>
2017-01-07 21:10:30 -0800

    - implemented some basic rss file parsing

--- RSSScraper.pm
+++ RSSScraper.pm
@@ -10,12 +10,15 @@ use HTML::TreeBuilder::XPath;
 use XML::XPath;
 use URI;
 
+use Data::Dumper;
+
+
 
 # <link rel="alternate" type="application/rss+xml" title="red|blue - RSS" href="http://www.redblue.team/feeds/posts/default?alt=rss" />
 
 
 
-sub extract_rss_link {
+sub get_rss_link {
 	my ($self, $url) = @_;
 
 	$self->get($url);
@@ -28,10 +31,42 @@ sub extract_rss_link {
 	return
 }
 
+sub get_rss_data {
+	my ($self, $rss_url) = @_;
+
+	$self->get($rss_url);
+
+	my %data;
+	my $xp = XML::XPath->new(xml => $self->content);
+	$data{title} = '' . $xp->findvalue('/rss/channel/title');
+	$data{description} = '' . $xp->findvalue('/rss/channel/description');
+	$data{link} = '' . $xp->findvalue('/rss/channel/link');
+	$data{managing_editor} = '' . $xp->findvalue('/rss/channel/managingEditor');
+	$data{build_date} = '' . $xp->findvalue('/rss/channel/lastBuildDate');
+	foreach my $category ($xp->findnodes('/rss/channel/category')) {
+		push @{$data{categories}}, '' . $category->findvalue('.');
+	}
+
+	foreach my $item ($xp->findnodes('/rss/channel/item')) {
+		my %item_data;
+		$item_data{title} = '' . $item->findvalue('./title');
+		$item_data{description} = '' . $item->findvalue('./description');
+		$item_data{link} = '' . $item->findvalue('./link');
+		$item_data{author} = '' . $item->findvalue('./author');
+		$item_data{pub_date} = '' . $item->findvalue('./pubDate');
+
+		push @{$data{items}}, \%item_data;
+	}
+
+	return \%data
+}
+
 
 sub main {
 	my $ua =RSSScraper->new;
-	say $ua->extract_rss_link('http://www.redblue.team/');
+	my $link = $ua->get_rss_link('http://www.redblue.team/');
+	say $link;
+	say Dumper $ua->get_rss_data($link);
 }
 
 caller or main(@ARGV)
